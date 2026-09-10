c9c4b648510ef07a5e513cbf9f1903fe61e39d92
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-10 12:48:02 +0200

    arg.lib.sh quote as only function, best possible and robust implementation.

--- cmd/lib/arg.lib.sh
+++ cmd/lib/arg.lib.sh
@@ -1,57 +1,8 @@
-# current_args="$(saveargs "$@")"
+# current_args="$(quote "$@")"
 # set -- foo bar baz boo
 # eval "set -- $current_args"
 
-# TODO evaluate the decision to use only one function with name "quote" that uses the current "saveargs", because: quote "$1" == saveargs "$1"
-
 quote()
-{
-  if [ "$#" -ne 1 ]
-  then
-    return 1
-  fi
-
-  (
-    _quote_arg="$1"
-
-    printf "%s" "'" || exit 1
-
-    while [ "$_quote_arg" != "${_quote_arg#*"'"}" ]
-    do
-      _quote_part="${_quote_arg%%"'"*}"
-
-      printf "%s%s" "$_quote_part" "'\\''" || exit 1
-
-      _quote_arg="${_quote_arg#*"'"}"
-    done
-
-    printf "%s%s" "$_quote_arg" "'" || exit 1
-  )
-}
-
-quoteargs()
-{
-  while [ "$#" -gt 0 ]
-  do
-    quote "$1" || return 1
-
-    shift
-
-    if [ "$#" -gt 0 ]
-    then
-      printf "%s" " " || return 1
-    fi
-  done
-}
-
-quotearg()
-{
-  [ "$#" -eq 1 ] || return 1
-
-  saveargs "$1"
-}
-
-saveargs()
 {
   (
     while [ "$#" -gt 0 ]
