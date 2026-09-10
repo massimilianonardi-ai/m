af04befd080092608ce67e5c1e4045ef4682fe17
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-10 12:15:05 +0200

    optimized

--- cmd/lib/arg.lib.sh
+++ cmd/lib/arg.lib.sh
@@ -16,50 +16,35 @@ quote()
 # current_args="$(saveargs "$@")"
 # set -- foo bar baz boo
 # eval "set -- $current_args"
-___saveargs()
-{
-  for i
-  do
-    printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/"
-  done
-  echo " "
-}
-
-___saveargs()
-{
-  for i
-  do
-    quote "$i"
-    printf " "
-  done
-}
-
-___saveargs()
-{
-  while [ "$#" -gt "0" ]
-  do
-    quote "$1"
-    shift
-    if [ "$#" -gt "0" ]
-    then
-      printf " "
-    fi
-  done
-}
 
 saveargs()
 {
-  while [ "$#" -gt "1" ]
-  do
-    quote "$1"
-    shift
-    printf " "
-  done
+  (
+    while [ "$#" -gt 0 ]
+    do
+      _saveargs_arg="$1"
 
-  if [ "$#" -gt "0" ]
-  then
-    quote "$1"
-  fi
+      printf "'" || exit
+
+      while [ "$_saveargs_arg" != "${_saveargs_arg#*"'"}" ]
+      do
+        _saveargs_part="${_saveargs_arg%%"'"*}"
+
+        printf "%s'\\\\''" "$_saveargs_part" || exit
+
+        _saveargs_arg="${_saveargs_arg#*"'"}"
+      done
+
+      printf "%s'" "$_saveargs_arg" || exit
+
+      shift
+
+      if [ "$#" -gt 0 ]
+      then
+        printf " " || exit
+      fi
+    done
+  )
 }
 
 #-------------------------------------------------------------------------------
